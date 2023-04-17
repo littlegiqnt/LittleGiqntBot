bf9469773a833fcd3468b28636da6fcb1c381fd6
littlegiqnt <hswhsw610@gmail.com>
2023-04-17 13:06:29 +0900

    fix: mongodb memory database version error

--- src/utils/createMongoServer.ts
+++ src/utils/createMongoServer.ts
@@ -1,6 +1,6 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 
 export default async () => {
-    const mongod = await MongoMemoryServer.create();
+    const mongod = await MongoMemoryServer.create({ binary: { version: "4.4.19" } });
     return mongod.getUri();
 };
