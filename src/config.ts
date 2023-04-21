b9d349fa34b9f4e2084b64ae840e680c02689803
littlegiqnt <hswhsw610@gmail.com>
2023-04-21 11:33:26 +0900

    change text command prefix

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const GUILD_ID = "1023072472712609873";
-export const PREFIX = "!";
+export const PREFIX = "??";
 export const NAME = "LittleGiqntBot";
 export const DB_URI = "mongodb://db/kuabot";
 export const DEBUG_COMMANDS = false;
