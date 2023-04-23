97dc888260a75f17b8784d948d14d09802a7cc3a
littlegiqnt <hswhsw610@gmail.com>
2023-04-23 17:01:28 +0900

    feat: add try/catch to eval command

--- src/textcommands/evaljs.ts
+++ src/textcommands/evaljs.ts
@@ -1,11 +1,30 @@
+import { EmbedBuilder, escapeMarkdown } from "discord.js";
 import { TextCommand } from "structure/TextCommand";
+import isProduction from "utils/isProduction";
 
 export default new TextCommand({
-    name: "eval",
+    const name: isProduction()
+        ? "eval"
+        : "testeval",
     async execute(msg, args) {
         if (msg.author.id !== "454927000490999809") {
             return;
         }
-        eval(args);
+        try {
+            eval(args);
+        } catch (e) {
+            const embed = new EmbedBuilder()
+                .setColor("Red")
+                .setTitle("오류 발생!");
+            if (e instanceof Error) {
+                embed.setDescription(escapeMarkdown(e.stack ?? ""));
+            } else {
+                embed.setDescription(escapeMarkdown(String(e)));
+            }
+
+            msg.reply({
+                embeds: [embed],
+            });
+        }
     },
 });
