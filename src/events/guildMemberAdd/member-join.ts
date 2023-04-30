50e7d21ccc78cfbde92ced50f3c61f968457f608
littlegiqnt <hswhsw610@gmail.com>
2023-04-30 18:05:24 +0900

    fix: remove logging other server's join/quit

--- src/events/guildMemberAdd/member-join.ts
+++ src/events/guildMemberAdd/member-join.ts
@@ -3,11 +3,19 @@ import logger from "structure/Logger";
 import rolesManager from "structure/RolesManager";
 import reloadMembersCount from "utils/reloadMembersCount";
 import createGuildMemberAddEventListener from "./createGuildMemberAddEventListener";
+import { GUILD_ID } from "config";
 
-export default createGuildMemberAddEventListener(async (member: GuildMember) => {
-    member.roles.add([rolesManager.get("dividerRoleProfile")!, rolesManager.get("dividerRoleNotice")!, rolesManager.get("dividerRoleGames")!, rolesManager.get("dividerRoleUser")!]);
+export default createGuildMemberAddEventListener(async (member) => {
+    // return if it is not my personal private server
+    if (member.guild.id !== GUILD_ID) return;
+
+    member.roles.add([
+        rolesManager.get("dividerRoleProfile")!,
+        rolesManager.get("dividerRoleNotice")!,
+        rolesManager.get("dividerRoleGames")!,
+        rolesManager.get("dividerRoleUser")!,
+    ]);
     reloadMembersCount(member.client);
-
     if (!member.pending) {
         logger.userJoin(member);
     }
--- src/events/guildMemberRemove/member-quit.ts
+++ src/events/guildMemberRemove/member-quit.ts
@@ -1,8 +1,12 @@
+import { GUILD_ID } from "config";
 import logger from "structure/Logger";
 import reloadMembersCount from "utils/reloadMembersCount";
 import createGuildMemberRemoveEventListener from "./createGuildMemberRemoveEventListener";
 
 export default createGuildMemberRemoveEventListener(async (member) => {
-    logger.userQuit(member);
+    // return if it is not my personal private server
+    if (member.guild.id !== GUILD_ID) return;
+
     reloadMembersCount(member.client);
+    logger.userQuit(member);
 });
