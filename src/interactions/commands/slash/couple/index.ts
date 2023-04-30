f012ce30a5419243986afaee892a6724a899faf8
littlegiqnt <hswhsw610@gmail.com>
2023-04-30 18:13:12 +0900

    fix: forgot to add the couple remove sub command

--- src/interactions/commands/slash/couple/index.ts
+++ src/interactions/commands/slash/couple/index.ts
@@ -1,11 +1,12 @@
 import { SlashCommand } from "structure/interaction/command/SlashCommand";
 import days from "./days";
 import since from "./since";
+import remove from "./remove";
 
 export default new SlashCommand({
     name: "couple",
     nameLocales: {
         ko: "커플",
     },
-    subCommands: [since, days],
+    subCommands: [since, days, remove],
 });
